58984fc1d603a9210d24f0fbc91cc9be1b010191
TrueMolni <truemolni@gmail.com>
2023-05-11 17:52:58 +0300

    0.1.0

---
+++ server.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = require("./app");
+
+const { DB_HOST, PORT = 3030 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(PORT))
+  .catch((error) => console.log(error.message));
