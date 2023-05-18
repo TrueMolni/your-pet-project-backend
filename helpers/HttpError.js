0bcf9f6a7db848774c9587d8c96d6263c17427ee
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-05-18 13:54:37 +0300

    Adds minor nuances

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,9 +1,11 @@
 const errorMessages = {
-  400: "Bad Request",
-  401: "Unauthorized",
-  403: "Forbidden",
-  404: "Not Found",
-  409: "Conflict",
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'Not Found',
+  409: 'Conflict',
+  410: 'Gone',
+  500: 'Server error',
 };
 
 const HttpError = (status, message = errorMessages[status]) => {
