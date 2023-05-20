b5e25dc999b322fc207d014c5b4a7da039f0e990
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-05-21 01:09:20 +0300

    updates via .prettierrc.json

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -1,14 +1,14 @@
-const express =require("express");
-const  tryCatchWrapper  =require("../../utils/tryCatchWrapper.js");
-const ctrls=require("../../controllers/auth-controllers.js");
-const authenticate  =require("../../middlewares/authenticate.js");
+const express = require('express');
+const tryCatchWrapper = require('../../utils/tryCatchWrapper.js');
+const ctrls = require('../../controllers/auth-controllers.js');
+const authenticate = require('../../middlewares/authenticate.js');
 const router = express.Router();
 
-router.post("/register",tryCatchWrapper(ctrls.ctrlRegisterUser));
-router.post("/login", tryCatchWrapper(ctrls.ctrlLoginUser));
-router.post("/logout", authenticate, tryCatchWrapper(ctrls.ctrlLogOut));
-router.get("/google",tryCatchWrapper(ctrls.googleAuth));
-router.get("/google-redirect",tryCatchWrapper(ctrls.googleRedirect));
-router.patch("/info", authenticate, tryCatchWrapper(ctrls.ctrlUserInfo));
+router.post('/register', tryCatchWrapper(ctrls.ctrlRegisterUser));
+router.post('/login', tryCatchWrapper(ctrls.ctrlLoginUser));
+router.post('/logout', authenticate, tryCatchWrapper(ctrls.ctrlLogOut));
+router.get('/google', tryCatchWrapper(ctrls.googleAuth));
+router.get('/google-redirect', tryCatchWrapper(ctrls.googleRedirect));
+router.patch('/info', authenticate, tryCatchWrapper(ctrls.ctrlUserInfo));
 
 module.exports = router;
