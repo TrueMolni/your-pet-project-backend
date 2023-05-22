44d47940b0445b8ab173fe4ea1a025d86d4d5520
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-05-22 14:53:53 +0300

    upd

--- routes/api/notices.js
+++ routes/api/notices.js
@@ -11,20 +11,17 @@ router.get('/', controller.getAllNotices); //! {видалити} 0 ендпоі
 
 router.get('/search', controller.getNoticesByTitle);
 router.get('/category/:categoryId', controller.getNoticesByCategory);
+router.get('/own', authenticate, controller.getUserNotices); // 8
+router.get('/favorites', authenticate, controller.getNoticeByFavorite); // 5 ендпоінт для отримання оголошень авторизованого користувача доданих ним же в обрані
 router.get('/:noticeId', controller.getNoticeById);
 router.patch('/favorite/:noticeId', authenticate, controller.updateFavorite); // 4-6
-// router.patch('/favorite/:noticeId', authenticate, validation(noticeSchema), asyncWrapper(controller.updateFavorite)); //! 4
-
-router.get('/favorites', authenticate, controller.getNoticeByFavorite); // 5 ендпоінт для отримання оголошень авторизованого користувача доданих ним же в обрані
-
+// router.patch('/favorite/:noticeId', authenticate, validation(noticeSchema), asyncWrapper(controller.updateFavorite)); //! 4-6 ?? validation(noticeSchema)
 router.post(
   '/notice',
   authenticate,
   uploadNotice.single('image'),
   controller.addNoticeByCategory
 ); // 7
-
-router.get('/own', authenticate, controller.getUserNotices); // 8
 router.delete('/:noticeId', authenticate, controller.deleteNoticeById); // 9
 
 module.exports = router;
