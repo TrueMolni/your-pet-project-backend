c5f1703b4b49c57aaad5293f2103913e2a62bc17
DonetsYuliya <donecjuli@gmail.com>
2023-05-24 01:08:38 +0300

    create news api

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const swaggerDocument = require('./swagger.json');
 
 const noticeRouter = require('./routes/api/notices');
 const MyPetRouter = require('./routes/api/myPet');
+const NewsRouter = require('./routes/api/news');
 const authRouter = require('./routes/api/auth-routes.js');
 const app = express();
 
@@ -23,6 +24,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/users', authRouter);
 app.use('/api/notices', noticeRouter);
 app.use('/api/pet', MyPetRouter);
+app.use('/api/news', NewsRouter);
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
