e240090531f8093678242c59184ceceac6d4bf9d
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-05-24 12:53:07 +0300

    removes unnecessary code

--- controllers/notices/2_getNoticesByCategory.js
+++ controllers/notices/2_getNoticesByCategory.js
@@ -3,15 +3,16 @@ const { Notice } = require('../../models/noticeModel');
 
 const getNoticesByCategory = async (req, res, next) => {
   const { categoryId } = req.params;
-  console.log('categoryId >>>>>>', categoryId);
-  //   const { categoryId } = req.query;
+
   const result = await Notice.find({
     category: categoryId,
   });
+
   if (result.length === 0) {
     throw new NotFound(`Notice with category=${categoryId} not found`);
   }
-  res.status(200).json({ result });
+
+  res.status(200).json({ count: result.length, data: result });
 };
 
 module.exports = getNoticesByCategory;
