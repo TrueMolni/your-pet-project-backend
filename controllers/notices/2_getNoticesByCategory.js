8fc20f38f840393235fe969d3672a8746516410f
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-05-24 14:19:41 +0300

    Adds sorting

--- controllers/notices/2_getNoticesByCategory.js
+++ controllers/notices/2_getNoticesByCategory.js
@@ -1,16 +1,26 @@
 const { NotFound } = require('http-errors');
+
+const { HttpError } = require('../../helpers');
 const { Notice } = require('../../models/noticeModel');
 
 const getNoticesByCategory = async (req, res, next) => {
   const { categoryId } = req.params;
 
-  const result = await Notice.find({
+  const unsortedNotices = await Notice.find({
     category: categoryId,
   });
 
-  if (result.length === 0) {
-    throw new NotFound(`Notice with category=${categoryId} not found`);
+  if (unsortedNotices.length === 0) {
+    throw new NotFound(`Notice with category "${categoryId}" not found`);
   }
+  if (!unsortedNotices) {
+    throw HttpError(404);
+  }
+
+  const result = [...unsortedNotices].sort(
+    (firstNotice, secondNotice) =>
+      new Date(secondNotice.createdAt) - new Date(firstNotice.createdAt)
+  );
 
   res.status(200).json({ count: result.length, data: result });
 };
