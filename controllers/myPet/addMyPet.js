d51636dc97673e9735e033013573a239f0e93832
TrueMolni <truemolni@gmail.com>
2023-05-26 12:30:22 +0300

    fix
    same fixes

--- controllers/myPet/addMyPet.js
+++ controllers/myPet/addMyPet.js
@@ -1,28 +1,30 @@
-const cloudinary = require('cloudinary').v2;
 const { MyPet } = require('../../models/myPetModel');
+const { HttpError } = require('../../helpers/index');
 const { tryCatchWrapper } = require('../../utils/index');
 
 const addMyPet = async (req, res) => {
+  const { name } = req.body;
+
+  const pet = await MyPet.findOne({ name });
+  if (pet) {
+    throw HttpError(409, `Pet ${name} already in use`);
+  }
+
   const { _id: owner } = req.user;
 
-  const { path } = req.file;
-
-  const { url } = await cloudinary.uploader.upload(path, {
-    width: 150,
-    height: 150,
-    crop: 'fill',
-    gravity: 'center',
-  });
-
   const newPet = req.file
-    ? { avatarURL: url, owner, ...req.body }
+    ? { avatarURL: req.file.path, owner, ...req.body }
     : { owner, ...req.body };
 
   const result = await MyPet.create({
     ...newPet,
   });
 
-  res.status(201).json(result);
+  res.status(201).json({
+    data: {
+      ...result._doc,
+    },
+  });
 };
 
 module.exports = { addMyPet: tryCatchWrapper(addMyPet) };
--- controllers/notices/7_addNoticeByCategory.js
+++ controllers/notices/7_addNoticeByCategory.js
@@ -1,31 +1,69 @@
-const cloudinary = require('cloudinary').v2;
-const fs = require('fs/promises');
-const { HttpError } = require('../../helpers');
+// const cloudinary = require('cloudinary').v2;
+// const fs = require('fs/promises');
+// const { HttpError } = require('../../helpers');
+// const { Notice } = require('../../models/noticeModel');
+
+// const addNoticeByCategory = async (req, res) => {
+//   const { _id: owner } = req.user;
+//   if (!req.file) {
+//     throw HttpError(400, 'Avatar is required');
+//   }
+//   console.log('req.file >>>>', req.file);
+
+//   const { path: tempUpload } = req.file;
+
+//   const { url } = await cloudinary.uploader.upload(tempUpload, {
+//     width: 600,
+//     height: 600,
+//     crop: 'fill',
+//     gravity: 'center',
+//   });
+//   const avatarURL = url;
+
+//   const result = await Notice.create({
+//     ...req.body,
+//     avatarURL,
+//     owner,
+//   });
+//   fs.unlink(tempUpload);
+
+//   res.status(201).json({ notice: result });
+// };
+
+// module.exports = addNoticeByCategory;
+
+// const cloudinary = require('cloudinary').v2;
+// const fs = require('fs/promises');
+
+// const { HttpError } = require('../../helpers');
 const { Notice } = require('../../models/noticeModel');
 
 const addNoticeByCategory = async (req, res) => {
   const { _id: owner } = req.user;
-  if (!req.file) {
-    throw HttpError(400, 'Avatar is required');
-  }
-  console.log('req.file >>>>', req.file);
 
-  const { path: tempUpload } = req.file;
+  // console.log('req >>>>>', req);
 
-  const { url } = await cloudinary.uploader.upload(tempUpload, {
-    width: 600,
-    height: 600,
-    crop: 'fill',
-    gravity: 'center',
-  });
-  const avatarURL = url;
+  // if (!req.file) {
+  //   throw HttpError(400, 'Avatar is required');
+  // }
+
+  const newNotice = req.file
+    ? { avatarURL: req.file.path, owner, ...req.body }
+    : { owner, ...req.body };
+
+  // const { path: tempUpload } = req.file;
+  // const { url } = await cloudinary.uploader.upload(tempUpload); // Завантаження тимчасового файлу на Cloudinary
+  // const avatarURL = url;
 
   const result = await Notice.create({
-    ...req.body,
-    avatarURL,
-    owner,
+    ...newNotice,
   });
-  fs.unlink(tempUpload);
+  // const result = await Notice.create({
+  //   ...req.body,
+  //   avatarURL,
+  //   owner,
+  // });
+  // fs.unlink(tempUpload); // Видалення тимчасового файлу з файлової системи
 
   res.status(201).json({ notice: result });
 };
